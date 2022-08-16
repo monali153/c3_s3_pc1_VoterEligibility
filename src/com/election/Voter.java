7fa54a1e90bb9a571d1bd51977025462d397e976
Monali Sharma <mona.chaudhari444@gmail.com>
2022-08-16 15:26:34 +0530

    completed task

---
+++ src/com/election/Voter.java
@@ -0,0 +1,40 @@
+package com.election;
+
+public class Voter {
+
+    static final int VOTER_ELIGIBLE_AGE = 18;
+    private String name;
+    private int age;
+
+    public Voter() {
+        name = null;
+        age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAgeCriteria(int age) {
+
+        if (age >= VOTER_ELIGIBLE_AGE) {
+            return getName() + " Is Eligible to Vote";
+        } else if (age > 0 && age < VOTER_ELIGIBLE_AGE) {
+            return getName() + " Is Not Eligible to Vote";
+        }
+        return "Age Can't Be Negative or Zero";
+
+    }
+}
---
+++ src/com/election/VoterImpl.java
@@ -0,0 +1,16 @@
+package com.election;
+
+public class VoterImpl {
+    public static void main(String[] args) {
+
+
+        Voter object = new Voter();
+
+        object.setName("John");
+        object.setAge(10);
+
+        System.out.println(object.getAgeCriteria(object.getAge()));
+    }
+
+
+}
